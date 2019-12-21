3c40407d16e2364f5f30a399a9128b14dbdf98d2
Danny Saro <Danny.Saro@ca.com>
2019-12-21 19:01:40 +0100

    Building out interface

--- SIWB_Repository/src/org/siwb/repository/IObjectCollection.java
+++ SIWB_Repository/src/org/siwb/repository/IObjectCollection.java
@@ -3,6 +3,7 @@ package org.siwb.repository;
 public interface IObjectCollection
 {
 
+	String getId();
 	String getName();
 
 }
---
+++ SIWB_Repository/src/org/siwb/repository/impl/FileBasedRepository.java
@@ -0,0 +1,32 @@
+package org.siwb.repository.impl;
+
+import java.io.File;
+
+import org.siwb.repository.IObjectCollection;
+import org.siwb.repository.IRepository;
+
+public class FileBasedRepository
+	implements IRepository
+{
+	private String name;
+	private File rootDir;
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean allowsObjectCollection(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean allowsObjectCollection(IObjectCollection oc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
