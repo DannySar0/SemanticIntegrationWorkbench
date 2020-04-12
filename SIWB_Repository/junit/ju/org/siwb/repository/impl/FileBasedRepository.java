8e51a1d26cec1a3e37d2e54acb344c47f312b73c
Danny Saro <Danny.Saro@ca.com>
2020-04-12 16:35:46 +0200

    Initial JUnit Test

---
+++ SIWB_Repository/junit/ju/org/siwb/repository/impl/FileBasedRepository.java
@@ -0,0 +1,26 @@
+package ju.org.siwb.repository.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileBasedRepository {
+	
+	
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllowsObjectCollectionString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllowsObjectCollectionIObjectCollection() {
+		fail("Not yet implemented");
+	}
+
+}
